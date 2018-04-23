ada2680241ec8bf8d8fad6cfa1b44b7532bbad5c
StephenSAE <1011091@student.sae.edu.au>
2018-04-23 23:28:23 +1000

    Added Mustachio animation and Started working on main menu
    Also fixed up Japan level colour problem and distinction between middle ground and background.

---
+++ Assets/QuitOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : StateMachineBehaviour
+{
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Quit();
+    }
+
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
