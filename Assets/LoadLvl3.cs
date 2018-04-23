0c31d3106ba3c2bcf5872e36787c2ba4890e70ee
StephenSAE <1011091@student.sae.edu.au>
2018-04-24 01:11:52 +1000

    Added sound and all submenus of the main menu as well as options menu functionality

---
+++ Assets/LoadLvl3.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLvl3 : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SceneManager.LoadScene(3);
+    }
+}
