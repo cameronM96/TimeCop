fdd6526dc7f0467940f0a32f6f9e21616e70f760
StephenSAE <1011091@student.sae.edu.au>
2018-04-24 20:43:18 +1000

    Created Pause menu prefab

---
+++ Assets/PauseRestarter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseRestarter : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+    }
+
+}
