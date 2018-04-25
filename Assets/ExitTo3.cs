cb4b7e0643ac44087f980079cff7fab23c0890e1
cdavis3108 <37386600+cdavis3108@users.noreply.github.com>
2018-04-26 00:07:55 +1000

    Revert "Revert "trying""
    This reverts commit eaf86767a81b92c119860251f5c413cbe6e89392.

---
+++ Assets/ExitTo3.cs
@@ -0,0 +1,13 @@
+﻿//using UnityEngine;
+//using System.Collections;
+
+//public class NextLevel : MonoBehaviour
+//{
+
+//    void OnTriggerEnter2D(Collider2D other)
+//    {
+//       // if (other.gameObject.tag == "Player")
+//         //   SceneManager.LoadScene("3");
+//    }
+
+//}
